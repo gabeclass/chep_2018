e84eebac2c7d2d586e850a98469db97d1e3320c8
Henry Fredrick Schreiner <henry.fredrick.schreiner@cern.ch>
2018-07-01 11:05:09 +0200

    Merging in submodule for later changes

---
+++ lhcb_presentation/README.md
@@ -0,0 +1,46 @@
+# LHCb Presentation style (with CookieCutter)
+
+This template provides a beamer style file, a beamer module, and a [Pandoc] template to use for making beautiful presentations simply. It is customized slightly for UC, but that is easy to replace with different branding.
+
+## Quick setup
+
+Get going fast with [cookiecutter]. Just use `pip install cookiecutter` to get the [cookiecutter] application. Then, run:
+
+```bash
+cookiecutter https://gitlab.cern.ch/hschrein/lhcb_presentation
+```
+You will be asked a few questions. Make sure you do not add an extension to `file_name`; it will do that for you. You can select either LaTeX (`tex`) or Markdown via [Pandoc] (`md`) as the `type`.
+It will make a new git repository, and check this repository out as a submodule. The basics will be prepared for you.
+
+
+## Customization
+
+The following is the built-in support for UC branding, you can customize it for your University (use `newcommmand` or `renewcommand` as necessary, in the style files `providecommand` is used):
+
+```tex
+\newcommand{\logomax}{\node [right] at (0,1.143) {\includegraphics[height=1.3cm]{logos/UC_logo_bb.pdf}};}
+\newcommand{\logomini}{\node at (1,1.143/2) {\includegraphics[height=.75cm]{logos/UC_logo_bb.pdf}};}
+\newcommand{\logowidth}{2.5}
+```
+
+To remove it:
+
+```tex
+\newcommand{\logomax}{}
+\newcommand{\logomini}{}
+\newcommand{\logowidth}{0}
+```
+
+## Builds
+
+You can run `./makeslides.py` if you have Plumbum (and Pandoc) installed, and all `.md` or `.tex` files will be processed if out of date. If the names match, the `.md` file takes preference.
+
+## Gitlab builds
+
+If you use GitLab, you can use the CI system to build your PDFs for you. An example file for builds is included when you make a new presentation, and the output is available at `https://gitlab.cern.ch/<user>/<reponame>/builds/artifacts/master/file/<file_name>.pdf?job=compile_pdf`.
+
+You might need a relative path or HTTPS path in `.gitmodules` to build (see [this page](https://docs.gitlab.com/ce/ci/git_submodules.html)). This is because GitLab CI uses HTTP(S) for all cloning. You'll get that option when the [cookiecutter] runs.
+
+
+[cookiecutter]:   https://cookiecutter.readthedocs.io/en/latest/
+[Pandoc]:         https://pandoc.org
---
+++ lhcb_presentation/makeslides.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from plumbum import local, cli, FG, colors, ProcessExecutionError
+from plumbum.path.utils import gui_open
+
+class Doc(object):
+    def __init__(self, filename, ext):
+        self.filename = filename
+        self.input = filename.with_suffix(ext)
+        self.output = filename.with_suffix('.pdf')
+        self.name = filename.with_suffix('').basename
+    def exists(self):
+        return self.input.exists()
+    def needs_update(self):
+        if not self.output.exists():
+            return True
+        return self.input.stat().st_mtime > self.output.stat().st_mtime
+    def open(self):
+        try:
+            gui_open(self.output)
+        except ProcessExecutionError:
+            pass
+
+class MakeSlides(cli.Application):
+    'Make slides from all available files in current directory'
+
+    @cli.positional(cli.ExistingFile)
+    def main(self, *filenames):
+        if not filenames:
+            filenames = local.cwd // ('*.md', '*.tex')
+            auto = True
+        else:
+            auto = False
+
+        for fname in filenames:
+            document = Doc(fname, '.md')
+            if document.exists() and (document.needs_update() or not auto):
+                colors.info.print("Making", document.name, "with pandoc")
+                local['pandoc']['-t', 'beamer',
+                        '--template=lhcb.beamer',
+                        document.input,
+                        '-o', document.output] & FG
+                document.open()
+            else:
+                document = Doc(fname, '.tex')
+                if document.exists() and document.needs_update():
+                    colors.info.print("Making", document.name, "with latex")
+                    local['latexmk']['-pdf', document.input] & FG
+                    document.open()
+
+        generated = ('*.aux', '*.bcf', '*.fls', '*.idx', '*.ind', '*.lof', '*.lot',
+                '*.out', '*.toc', '*.blg', '*.ilg', '*.log',
+                'aux.bak', 'idx.bak', 'texput.log', 'texput.aux',
+                '*.synctex.gz*', '*.nav', '*.snm',
+                '*.fdb_latexmk')
+
+
+        for gen in generated:
+            files = (local.cwd // gen)
+            if files:
+                for f in files:
+                    f.delete()
+
+if __name__ == '__main__':
+    MakeSlides()
